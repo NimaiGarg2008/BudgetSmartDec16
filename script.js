1482427a4c08f726e54794d788edc11af624bc41
NimaiGarg2008 <147553281+NimaiGarg2008@users.noreply.github.com>
2024-12-17 12:51:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Simple function to redirect the user to a different html webpage. We use location.replace so the user can not use the back button on their browser to go back and must navigate with buttons
+/* Simple function to redirect the user to a different html webpage.
+We use location.replace so the user can not use the back button on their browser
+to go back and must navigate with buttons */
 function goToNextPage(nextPage)
 {
             location.replace(nextPage);
